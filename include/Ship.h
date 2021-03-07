63bc4a8488fa0ce012175e7c37fd83854dce51de
Dolayo <44551274+Dolayo@users.noreply.github.com>
2021-03-07 20:41:22 +0100

    Continua en las ecuaciones de Newton

--- include/Ship.h
+++ include/Ship.h
@@ -1,12 +1,6 @@
 #ifndef __SHIP_H_
 #define __SHIP_H_
-/*
-#include "C:\mrcore\include\math\transformation2d.h"
 
-#include "C:\mrcore\include\sim\world.h"
-#include "C:\mrcore\include\sim\cylindricalpart.h"
-#include "C:\mrcore\include\sim\composedentity.h"
-*/
 #include <mrcore.h>
 
 
@@ -19,8 +13,9 @@ namespace mr
 		// Constructor
 		Ship(double w, double l, double m, double c);
 		
-
-		virtual bool move(double t, double y);
+		virtual bool move(double t, double r);
+		double coeff(const double& x, const char& fluid, const char& type);
+		double sdl(const double& x, const char& fluid, const char& type, const char& type2);
 		virtual bool getPose3D(Pose3D& pose) { pose = getAbsoluteT3D(); return true; }
 	
 		virtual void setLocation(const Transformation3D& p)
@@ -34,27 +29,51 @@ namespace mr
 
 		// Destructor
 		virtual ~Ship(){}
-	private:
+	protected:
+
+		// --- Ship attributes ---
 
 		double _width;
 		double _length;
 		double _mass;
-		double _I;
-		double _coeff;
-		double _speed;
-		double _rot_speed;
+		double _J;
+		double _xp;
 
-		// State
-		double _thrust;
-		double _yaw;//cambiar por timon
+		// --- Environment ---
+
+		double _trueWindDirection;
+		double _trueWaterDirection;
+		
+		double _windSpeed;
+		double _waterSpeed;
+		
+
+		// --- State ---
+
+		// Velocities
+		double _u;
+		double _v;
+		double _w;
+
+		// Position
+		double _x;
+		double _y;
+
+		// Orientation
+		double _yaw;
+
+		// Inputs
+		double _thrust_x;
+		double _thrust_y;
+	
 
 		//utility bool that checks if last move was possible (not collision, not off ground)
 		bool _move_success;
 
-		
-		void computeInertia();
-
 		// Default Constructor and operators
+
+	private:
+
 		Ship();
 		Ship& operator =(const Ship& s);
 		Ship(Ship&);
